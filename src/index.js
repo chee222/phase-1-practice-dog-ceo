797460bc72c2170cee8be6d24f3b2c3af0f343e3
Chee <ck_white@outlook.com>
2024-04-27 16:20:18 -0400

    dog

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 console.log('%c HI', 'color: firebrick')
+const imgUrl= Dog
+
+fetch('https://dog.ceo/api/breeds/image/random/4')
+  .then(response => response.json())
+  .then(data => {
+    // Handle the response data here
+  })
+  .catch(error => {
+    // Handle any errors that occur during the fetch
+  });
